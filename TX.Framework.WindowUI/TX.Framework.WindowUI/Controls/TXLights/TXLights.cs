03e398fc4fd9711a17edca33323ff4238ce78d78
xin liu <xinliuchnsz@gmail.com>
2020-10-07 09:33:43 +0800

    添加TXLights：指示灯

---
+++ TX.Framework.WindowUI/TX.Framework.WindowUI/Controls/TXLights/TXLights.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace TX.Framework.WindowUI.Controls {
+    [Description("指示灯")]
+    public partial class TXLights : UserControl {
+        protected Color centerColor = Color.White;
+        protected Color lightColor = Color.Green;
+        protected Color darkColor = Color.Gray;
+        private Color surroundColor = Color.Gray;
+        private Color currentColor = Color.Gray;
+        private bool turnOn = false;
+        private int interval = 0;
+
+        [Browsable(true)]
+        [Category("控制")]
+        [Description("闪烁间隔(ms)")]
+        public int Interval {
+            get { return interval; }
+            set {
+                if (value > 0) {
+                    twinkleTimer.Interval = value;
+                    interval = value;
+                }
+                else {
+                    interval = 0;
+                }
+                UpdateTimerStatus();
+            }
+        }
+
+        [Browsable(true)]
+        [Category("控制")]
+        [Description("是否显示指示灯边框,修改Padding.All可调整边框绘制")]
+        public bool IsDrawBorder { get; set; }
+
+        [Browsable(true)]
+        [Category("控制")]
+        [Description("是否渐变显示点亮状态，修改CenterColor可调整中心颜色")]
+        public bool IsGradient { get; set; }
+
+        [Browsable(true)]
+        [Category("控制")]
+        [Description("开/关指示灯")]
+        public bool TurnOn {
+            get {
+                return turnOn;
+            }
+            set {
+                turnOn = value;
+                UpdateTimerStatus();
+            }
+        }
+
+        [Browsable(true)]
+        [Category("控制")]
+        [Description("指示灯点亮后的颜色")]
+        public virtual Color LightColor {
+            get { return lightColor; }
+            set { lightColor = value; }
+        }
+
+        [Browsable(true)]
+        [Category("控制")]
+        [Description("指示灯熄灭后的颜色")]
+        public virtual Color DarkColor {
+            get { return darkColor; }
+            set { darkColor = value; }
+        }
+
+        [Browsable(true)]
+        [Category("控制")]
+        [Description("指示灯中心颜色")]
+        public virtual Color CenterColor {
+            get { return centerColor; }
+            set { centerColor = value; }
+        }
+
+        private Color SurroundColor {
+            get {
+                return LightColor;
+            }
+        }
+
+        private Color CurrentColor {
+            get {
+                if (Interval > 0) {
+                    currentColor = (TurnOn && currentColor == DarkColor) ? SurroundColor : DarkColor;
+                }
+                else {
+                    currentColor = TurnOn ? SurroundColor : DarkColor;
+                }
+                return currentColor;
+            }
+        }
+
+        public TXLights() {
+            InitializeComponent();
+        }
+
+        private void UpdateTimerStatus() {
+            if (turnOn && interval > 0) {
+                twinkleTimer.Enabled = true;
+            }
+            else {
+                twinkleTimer.Enabled = false;
+            }
+        }
+
+        protected virtual void Light_Paint(object sender, PaintEventArgs e) {
+            int padding = this.IsDrawBorder ? Padding.All : 0;
+            int width = Width - padding;
+            int height = Height - padding;
+            if (this.IsGradient && this.TurnOn) {
+                // 绘制中心渐变效果的圆形图案
+                using (GraphicsPath path = new GraphicsPath()) {
+                    path.AddEllipse(0, 0, Width, Height);
+                    PathGradientBrush pthGrBrush = new PathGradientBrush(path);
+                    pthGrBrush.CenterColor = CenterColor;
+                    Color[] colors = { CurrentColor };
+                    pthGrBrush.SurroundColors = colors;
+                    e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+                    e.Graphics.FillEllipse(pthGrBrush, 0, 0, width, height);
+                }
+            }
+            else {
+                e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+                e.Graphics.FillEllipse(new SolidBrush(this.CurrentColor), 0, 0, width, height);
+            }
+
+            if (this.IsDrawBorder) {
+                e.Graphics.DrawEllipse(Pens.Black, 0, 0, width, height);
+            }
+        }
+
+        protected override void OnPaint(PaintEventArgs e) {
+            base.OnPaint(e);
+            if (!this.IsDrawBorder) {
+                using (GraphicsPath path = new GraphicsPath()) {
+                    // 绘制圆形控件背景框
+                    path.AddEllipse(0, 0, Width, Height);
+                    this.Region = new Region(path);
+                }
+            }
+        }
+
+        protected override CreateParams CreateParams {
+            get {
+                var parms = base.CreateParams;
+                parms.Style &= ~0x02000000;  // Turn off WS_CLIPCHILDREN
+                return parms;
+            }
+        }
+
+        private void TwinkleTimer_Tick(object sender, EventArgs e) {
+            this.Invalidate();
+        }
+    }
+}
---
+++ TX.Framework.WindowUI/TX.Framework.WindowUI/Controls/TXLights/TXLights.designer.cs
@@ -0,0 +1,51 @@
+﻿namespace TX.Framework.WindowUI.Controls {
+    partial class TXLights {
+        /// <summary>
+        /// 必需的设计器变量
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.twinkleTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // twinkleTimer
+            // 
+            this.twinkleTimer.Tick += new System.EventHandler(this.TwinkleTimer_Tick);
+            // 
+            // TXLights
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Transparent;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "TXLights";
+            this.Padding = new System.Windows.Forms.Padding(2);
+            this.Size = new System.Drawing.Size(32, 32);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Light_Paint);
+            this.ResumeLayout(false);
+
+        }
+        #endregion
+
+        private System.Windows.Forms.Timer twinkleTimer;
+    }
+}
